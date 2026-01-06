52c25e74f54d083b932fec0ea1210ca1e38a3244
Dmytro Lukianchuk <dmytro.lukianchuk.lead@gmail.com>
2026-01-06 16:17:16 +0100

    add letsson9 (classes) and update package json

---
+++ JS fundamentals/lesson9.js
@@ -0,0 +1,12 @@
+
+// import { CustomerDetails } from "./printedHelper.js";
+
+// var customerDetails = new CustomerDetails();
+
+// customerDetails.printFirstName("John");
+// customerDetails.printLastName("Doe");
+
+import { customerDetails } from "./printedHelper.js";
+
+customerDetails.printFirstName("John");
+customerDetails.printLastName("Doe");
--- JS fundamentals/printedHelper.js
+++ JS fundamentals/printedHelper.js
@@ -3,3 +3,35 @@
 export function printHelper(message) {
     console.log("Helper says: " + message);
 }
+
+// Classes
+
+// export class CustomerDetails {
+
+//     printFirstName(firstName){
+//         console.log("First Name: " + firstName);
+//     }
+
+//     printLastName(lastName){
+//         console.log("Last Name: " + lastName);
+//     }
+// }
+
+
+class CustomerDetails {
+
+
+    /**
+     * This method will print the first name
+     * @param {string} firstName 
+     */
+    printFirstName(firstName){
+        console.log("First Name: " + firstName);
+    }
+
+    printLastName(lastName){
+        console.log("Last Name: " + lastName);
+    }
+}
+
+export const customerDetails = new CustomerDetails();
