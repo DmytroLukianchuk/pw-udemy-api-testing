953c37a8ad79109051f858864916706885485e47
Dmytro Lukianchuk <dmytro.lukianchuk.lead@gmail.com>
2026-01-06 16:54:55 +0100

    add last lesson and ts config to run ts files

---
+++ JS fundamentals/lesson10.ts
@@ -0,0 +1,24 @@
+// The diference between JS and TS variables
+
+var customerFirstName = "John";
+var customerLastName = "Doe";
+var customerAge = 30;
+var isPremiumMember = true;
+
+// this one will not work becaue of types
+// customerFirstName = 123;
+
+// Defining the type of variable
+var customerFirstName1 = "John";
+var customerLastName = "Doe";
+var customerAge = 30;
+var isPremiumMember = true;
+
+
+// Creating your own types
+var firstCustomer = {
+    firstName: "Alice",
+    lastName: "Smith",
+    age: 28,
+    active: true
+};
